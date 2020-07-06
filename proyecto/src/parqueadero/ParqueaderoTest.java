1f22822b9eb3e1bc340cf945938b0198ec71e569
joseperez33 <66077946+joseperez33@users.noreply.github.com>
2020-07-06 06:45:38 -0500

    Actualizaión

--- proyecto/src/parqueadero/ParqueaderoTest.java
+++ proyecto/src/parqueadero/ParqueaderoTest.java
@@ -1,13 +1,18 @@
 package parqueadero;
 
-import java.io.ByteArrayInputStream;
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-import java.io.InputStream;
-import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ * @author Jhonattan.Diaz
+ * @author Jose.Perez
+ * @author Mariano.Jara
+ * 
+ * @since 01/07/2020
+ * @version 1
+ *
+ */
 
 class ParqueaderoTest {
 
@@ -15,7 +20,6 @@ class ParqueaderoTest {
 
 	@Test
 	void testValidaParqueadero() throws Exception {
-		Robot robot = new Robot();
 		String nroPlaca ="";
 		String horaEntrada="",horasalida="";
 		int tipoveh=0,estado=0;		
@@ -26,27 +30,7 @@ class ParqueaderoTest {
 		rp.iniciar();	
 		Parqueadero p = new Parqueadero(nroPlaca, horaEntrada, horasalida, tipoveh, estado);
 		p.menu();
-		//p.menuPrincipal();
-		/*robot.keyPress(KeyEvent.VK_1);
-        robot.keyRelease(KeyEvent.VK_1);
-        robot.keyPress(KeyEvent.VK_ENTER);
-        robot.keyRelease(KeyEvent.VK_ENTER);*/
 		rp.ingresoVeh();		
-		// Simulate a key press
-       /*robot.keyPress(KeyEvent.VK_V);
-       robot.keyRelease(KeyEvent.VK_V);
-       robot.keyPress(KeyEvent.VK_D);
-       robot.keyRelease(KeyEvent.VK_D);
-       robot.keyPress(KeyEvent.VK_F);
-       robot.keyRelease(KeyEvent.VK_F);
-       robot.keyPress(KeyEvent.VK_4);
-       robot.keyRelease(KeyEvent.VK_4);
-       robot.keyPress(KeyEvent.VK_5);
-       robot.keyRelease(KeyEvent.VK_5);
-       robot.keyPress(KeyEvent.VK_6);
-       robot.keyRelease(KeyEvent.VK_6);
-       robot.keyPress(KeyEvent.VK_ENTER);
-       robot.keyRelease(KeyEvent.VK_ENTER);*/
 		rp.escrituraArchivo();
 		rp.SalidaVeh();
 		p.menuPrincipal();
